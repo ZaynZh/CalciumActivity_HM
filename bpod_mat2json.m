35b379bb3b96f408624d72d44459b6814e3bb19f
ZaynZh <142407158+ZaynZh@users.noreply.github.com>
2024-11-15 13:58:25 +0800

    Initial commit

---
+++ bpod_mat2json.m
@@ -0,0 +1,19 @@
+%% Export bpod data as .JSON
+animal_path = "/Users/fgs/HMLworkplace/Bpod_data/ZZ0024-L";
+animal_path = '/Volumes/Hiroshi Makino/User/KinOn/Data/diversity_training/bpod/ZZ0024-LR';
+output_path = "/Users/fgs/HMLworkplace/Arena_analysis/Data/bpod_data";
+sess_list = dir(animal_path);
+sess_list = sess_list(~ismember({sess_list.name}, {'.', '..','.DS_Store'}));
+for j = 1:length(sess_list)
+    sess_path = fullfile(animal_path, sess_list(j).name, "Session Data");
+    bpod_data_list = dir(sess_path);
+    bpod_data_list = bpod_data_list(~ismember({bpod_data_list.name}, {'.', '..','.DS_Store'}));
+    for i = 1:length(bpod_data_list)
+        bpod_data = load(fullfile(bpod_data_list(i).folder, bpod_data_list(i).name));
+        bpod_data = jsonencode(bpod_data);
+        output_filename = strrep(bpod_data_list(i).name, 'mat', 'json');
+        fid = fopen(fullfile(output_path, output_filename),'w');
+        fprintf(fid,'%s',bpod_data);
+        fclose(fid);
+    end
+end
---
+++ ws2pyreadable.m
@@ -0,0 +1,42 @@
+%% Export wavesurfer synchronized miniscope and bpod frames
+fs = 20000;
+
+dpath = '/Users/fgs/HMLworkplace/Arena_analysis/Data/ws_data/raw';
+dlist = dir(dpath);
+dlist = dlist(~ismember({dlist.name}, {'.', '..','.DS_Store'}));
+
+for i = 1:length(dlist)
+    recorded_data = ws.loadDataFile(fullfile(dpath,dlist(i).name));
+    
+    if isfield(recorded_data,'sweep_0001')
+        signal_series = recorded_data.sweep_0001.analogScans;
+    elseif isfield(recorded_data,'sweep_0002')
+        signal_series = recorded_data.sweep_0002.analogScans;
+    elseif isfield(recorded_data,'sweep_0003')
+        signal_series = recorded_data.sweep_0003.analogScans;
+    end
+    
+    
+    ms_signal_seq = signal_series(:,1);
+    bpod_signal_seq = signal_series(:,2);
+    
+    ms_signal_clean = abs(diff(ms_signal_seq));
+    ms_signal_clean(ms_signal_clean <= 2) = 0;
+    ms_signal_clean(ms_signal_clean > 2) = 1;
+    
+    ms_frames_samplingstamps = find(ms_signal_clean==1);
+    frames_ITI = diff(ms_frames_samplingstamps);
+    
+    bpod_signal_clean = diff(bpod_signal_seq);
+    bpod_signal_clean(bpod_signal_clean <= 2) = 0;
+    bpod_signal_clean(bpod_signal_clean > 2) = 1;
+    
+    bpod_trialstart_samplingstamps = find(bpod_signal_clean == 1);
+
+    output_filename = strrep(dlist(i).name, 'h5', 'mat');
+    ms_frames_filename = regexprep(output_filename,'_000(\w+).','_ms.');
+    bpod_trialstart_filename = regexprep(output_filename,'_000(\w+).','_bpod.');
+    output_path = strrep(dpath, 'raw', 'processed');
+    save(fullfile(output_path,ms_frames_filename),'ms_frames_samplingstamps');
+    save(fullfile(output_path,bpod_trialstart_filename),'bpod_trialstart_samplingstamps')
+end
